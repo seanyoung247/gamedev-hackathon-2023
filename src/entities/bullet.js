8359b18e8de87dd6f5609ca9cc05b023809bbb5c
sean <seanyoung247@gmail.com>
2023-06-20 18:42:52 +0100

    feat: Adds bullet actor

---
+++ src/entities/bullet.js
@@ -0,0 +1,11 @@
+import { Actor } from "./actor.js";
+
+export class Bullet {
+    constructor(scene, direction, rect) {
+        super(scene, rect, '');
+    }
+
+    update() {
+        
+    }
+}
